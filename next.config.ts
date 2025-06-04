a08693841cf3cb7345d81ddb67bfeeb2b5751103
Bifr <bimaibnufarrelrafael061@gmail.com>
2025-06-04 23:51:24 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 // next.config.js
 const securityHeaders = [
-
+  {
+    key: "Content-Security-Policy",
+    value:
+      "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' data: blob:; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob:; font-src 'self' https: data:; object-src 'none'; connect-src *; frame-ancestors 'self';",
+  },
   {
     key: "X-Frame-Options",
     value: "SAMEORIGIN",
